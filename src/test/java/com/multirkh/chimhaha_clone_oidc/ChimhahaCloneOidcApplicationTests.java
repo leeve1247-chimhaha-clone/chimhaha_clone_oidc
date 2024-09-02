a8a3478b4a3d4cef92f989e43d80945a4f7ef3b1
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-02 21:23:35 +0900

    Init project

---
+++ src/test/java/com/multirkh/chimhaha_clone_oidc/ChimhahaCloneOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multirkh.chimhaha_clone_oidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimhahaCloneOidcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
