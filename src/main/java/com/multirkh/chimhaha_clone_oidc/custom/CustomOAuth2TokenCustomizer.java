24e4df8a031ae54174760d09549bc5ac4fb654bc
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-04 19:22:22 +0900

    Update CustomOAuth2TokenCustomizer.java

--- src/main/java/com/multirkh/chimhaha_clone_oidc/custom/CustomOAuth2TokenCustomizer.java
+++ src/main/java/com/multirkh/chimhaha_clone_oidc/custom/CustomOAuth2TokenCustomizer.java
@@ -8,11 +8,14 @@ import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
+import java.time.Instant;
+
 
 @Configuration
 @RequiredArgsConstructor
 public class CustomOAuth2TokenCustomizer implements OAuth2TokenCustomizer<JwtEncodingContext> {
     private final UserEntityRepository userEntityRepository;
+
     @Override
     public void customize(JwtEncodingContext context) {
         if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
@@ -20,7 +23,9 @@ public class CustomOAuth2TokenCustomizer implements OAuth2TokenCustomizer<JwtEnc
             System.out.println("principalName = " + principalName);
             UserEntity byUserId = userEntityRepository.findByUserId(principalName);
             context.getClaims().claims((claims) -> {
-                claims.put("roles",byUserId.getUserRole().toString() );
+                claims.put("roles", byUserId.getUserRole().toString());
+                Instant iat = (Instant) claims.get("iat");
+                claims.put("exp", iat.plusSeconds(15));
             });
         }
     }
